6870d554ad1b4481c4b99afa8a50c0241b279447
Catherine Al Skaff <catherine.alskaf@gmail.com>
2023-03-19 23:01:47 +0100

    another edit

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const engine = require('ejs-mate');
 const dotenv= require('dotenv').config()
 const mongoose= require('mongoose')
 const Contact= require('./models/contacts');
+const session = require('express-session');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+ const MongoStore = require('connect-mongo');
+
+
+
+
 
 const app = express();
 app.engine('ejs', engine);
@@ -16,6 +24,85 @@ app.use(express.static(path.join(__dirname, 'public')));
 const dbUrl= process.env.DB_URL
 mongoose.set('strictQuery', false);
 mongoose.connect(dbUrl);
+
+
+
+
+
+const store= MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 3600 
+})
+
+store.on('error', (e)=>{
+   console.log('session store error', e)
+})
+
+const secret= process.env.SECRET || 'donttellsecret'
+
+const sessionConfig = {
+ store: store,
+   secret,
+   resave: false,
+   cookie: {
+       httpOnly: true,
+       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+       maxAge: 1000 * 60 * 60 * 24 * 7
+   }
+}
+app.use(session(sessionConfig));
+// app.use(flash());
+// app.use(
+//    helmet()
+//  );
+
+ const scriptSrcUrls = [
+   "https://stackpath.bootstrapcdn.com/",
+   "https://cdnjs.cloudflare.com/",
+   "https://cdn.jsdelivr.net",
+   "https://kit.fontawesome.com/bde2551b4f.js",
+   "https://cdn.fontawesome.com"
+
+];
+const styleSrcUrls = [ 
+   "https://kit-free.fontawesome.com/" ,
+   "https://stackpath.bootstrapcdn.com/" ,
+   "https://fonts.googleapis.com/" ,
+   "https://cdnjs.cloudflare.com",
+   "https://use.fontawesome.com/" ,
+   "https://cdn.jsdelivr.net" ,
+   "https://kit.fontawesome.com/",
+   "https://cdn.fontawesome.com"
+
+];
+const connectSrcUrls = [
+];
+const fontSrcUrls = [  
+    "https://fonts.gstatic.com/",
+"https://kit.fontawesome.com/"
+];
+app.use(
+   helmet.contentSecurityPolicy({
+       directives: {
+           defaultSrc: [],
+           connectSrc: ["'self'", ...connectSrcUrls],
+           scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+           styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+           workerSrc: ["'self'", "blob:"],
+           objectSrc: [],
+           imgSrc: [
+               "'self'",
+               "blob:",
+               "data:",
+               "https://res.cloudinary.com/dcvwaxbeh/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+               "https://images.unsplash.com/"
+           ],
+           fontSrc: ["'self'", ...fontSrcUrls],
+       },
+   })
+ );
+
+
 
 
 app.get('/', (req, res)=>{
